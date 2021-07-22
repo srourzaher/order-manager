3c6e8f67f6c87fabf4ed1f81236173a67b145a41
unknown <srourzaher@yahoo.fr>
2021-07-22 12:43:30 +0300

    first commit

---
+++ order-gateway/src/main/java/com/order/gatway/CloudGatewayApp.java
@@ -0,0 +1,15 @@
+package com.order.gatway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class CloudGatewayApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudGatewayApp.class, args);
+	}
+
+}
